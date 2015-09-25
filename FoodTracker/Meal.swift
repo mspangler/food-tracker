850aa6ebc7e1c38f5940b1d5f72c9dfce00576d3
Mark Spangler <mspangler@gmail.com>
2015-09-25 16:59:51 -0500

    defined data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Mark Spangler on 9/25/15.
+//  Copyright © 2015 Mark Spangler. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if self.name.isEmpty || self.rating < 0 {
+            return nil
+        }
+    }
+}
